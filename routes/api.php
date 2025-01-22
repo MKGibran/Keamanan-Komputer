cdf1d45c36ce9446fb6ae33f285746cd88643792
kgibran95 <kgibran95@gmail.com>
2025-01-22 19:48:19 +0700

    add api for brute force testing

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileUploadController;
+
+Route::get('files-download/{id}', [FileUploadController::class, 'download'])->name('files.download');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
